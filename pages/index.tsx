e271677bcfa83f37218a3076ab54912a078164f3
Shih-Yuan <shmily7829@gmail.com>
2023-04-26 19:37:13 +0800

    refactor and split component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,23 @@
-import SetPlayerNumberRequest from "@/api/data/request/SetPlayerNumberRequest";
 import GameStateData from "@/api/data/response/GameStateData";
 import GameStateEnum from "@/api/entities/GameStateEnum";
-import ApiContext from "@/components/ApiContext";
+import { useApiClients } from "@/components/ApiClientContext";
+import GamePlayScreen from "@/components/GamePlayScreen";
+import GameStartScreen from "@/components/GameStartScreen";
 import { useRouter } from "next/router";
-import {
-  useContext,
-  useState,
-  useEffect,
-  SetStateAction,
-  ChangeEventHandler,
-} from "react";
+import { useState, useEffect } from "react";
 
 export default function HomePage() {
   /*Context*/
-  const { gameController } = useContext(ApiContext);
+  const { game: gameApiClient } = useApiClients();
 
   /*State*/
   const [playerId, setPlayerId] = useState("");
-  const [number, setNumber] = useState("");
-  const [guess, setGuess] = useState("");
   const [gameState, setGameState] = useState<GameStateData | null>(null);
   const router = useRouter();
 
-  const handleClick = async () => {
-    const gameResponse = await gameController.createGame();
-    const gameStateResponse = await gameController.getGameState(
+  const startGame = async () => {
+    const gameResponse = await gameApiClient.createGame();
+    const gameStateResponse = await gameApiClient.getGameState(
       gameResponse.data.gameId
     );
     setPlayerId(gameStateResponse.data.player1Id);
@@ -35,19 +28,16 @@ export default function HomePage() {
     );
   };
 
-  //P2的畫面
   useEffect(() => {
     var playerId = router.query.playerId;
     var gameId = router.query.gameId;
 
     const fetchData = async () => {
-      //拿到遊戲狀態
       if (typeof gameId == "string") {
-        const gameStateResponse = await gameController.getGameState(gameId);
+        const gameStateResponse = await gameApiClient.getGameState(gameId);
         setGameState(gameStateResponse.data);
       }
 
-      //取得P2的Id
       if (typeof playerId == "string") {
         setPlayerId(playerId);
       }
@@ -69,7 +59,7 @@ export default function HomePage() {
 
     const fetchData = async () => {
       console.log(gameState);
-      const gameStateResponse = await gameController.getGameState(
+      const gameStateResponse = await gameApiClient.getGameState(
         gameState.gameId
       );
 
@@ -81,92 +71,37 @@ export default function HomePage() {
     }, 5000);
   }, [gameState]);
 
-  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setNumber(event.target.value);
-  }
-
-  async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    console.log("遊戲狀態:", gameState);
-    if (gameState == null) {
-      return;
-    }
-
-    if (gameState.gameState == GameStateEnum.SETTING_ANSWER) {
-      const playerAnsResponse = await gameController.setPlayerNumber(
-        { number },
-        gameState.gameId,
-        playerId
-      );
-      console.log(`${playerId}:`, playerAnsResponse);
-    }
-
-    if (gameState.gameState == GameStateEnum.GUESSING) {
-      const playerAnsResponse = await gameController.guessPlayerNumber(
-        { guesserId: playerId, number },
-        gameState.gameId
-      );
-
-      const gameStateResponse = await gameController.getGameState(
-        gameState.gameId
-      );
-
-      setGameState(gameStateResponse.data);
-
-      console.log(`${playerId}:${playerAnsResponse.message}`);
-    }
-  }
   if (gameState == null) {
-    return (
-      <div>
-        <h1>1A2B練功場</h1>
-        <button onClick={handleClick}>StartGame</button>
-      </div>
+    return <GameStartScreen onGameStart={startGame} />;
+  }
+
+  async function playerSetNumber(number: string) {
+    await gameApiClient.setPlayerNumber(
+      { number },
+      gameState!.gameId,
+      playerId
     );
   }
 
-  //P1的畫面
-  return (
-    <div>
-      <h1>1A2B練功場</h1>
-      {/* <pre>顯示遊戲狀態 {JSON.stringify(gameState, null, 2)}</pre> */}
-      <p>玩家id: {playerId}</p>
-      <p>設置的答案: {number}</p>
-      <p>遊戲狀態:{gameState.gameState}</p>
-      <p>當前玩家id: {gameState.turnPlayerId}</p>
-      <p>贏家：{gameState.winnerId}</p>
-      <br />
-      <form onSubmit={handleSubmit}>
-        <label>
-          Answer:
-          <input
-            type="text"
-            required
-            maxLength={4}
-            placeholder="請設置一組不重複的答案"
-            value={number}
-            onChange={handleInputChange}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      猜測記錄：
-      <textarea disabled value={toGameHistory(gameState).join("\n")}></textarea>
-    </div>
-  );
-}
+  async function playerGuessNumber(number: string) {
+    await gameApiClient.guessPlayerNumber(
+      { guesserId: playerId, number },
+      gameState!.gameId
+    );
 
-function toGameHistory(gameState: GameStateData): string[] {
-  const gameHistory: string[] = [];
-  for (let index = 0; index < gameState.guessHistory.length; index++) {
-    const guessNumber = gameState.guessHistory[index];
-    const guessResult = gameState.resultHistory[index];
+    const gameStateResponse = await gameApiClient.getGameState(
+      gameState!.gameId
+    );
 
-    if (index % 2 == 0) {
-      gameHistory.push(`player1 ${guessNumber} ${guessResult}`);
-    } else {
-      gameHistory.push(`player2 ${guessNumber} ${guessResult}`);
-    }
+    setGameState(gameStateResponse.data);
   }
-  return gameHistory;
+
+  return (
+    <GamePlayScreen
+      gameState={gameState}
+      playerId={playerId}
+      onPlayerSetNumber={playerSetNumber}
+      onPlayerGuessNumber={playerGuessNumber}
+    />
+  );
 }
